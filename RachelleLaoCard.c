3be224011f032d29ebdba1ec031b9119474a2a65
rachellelao <rachelle_lao@dlsu.edu.ph>
2016-05-24 16:32:33 +0800

    Create RachelleLaoCard.c

---
+++ RachelleLaoCard.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	puts("\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	puts("");
+	puts("\t   Rachelle Nicole A. Lao");
+	puts("\t\t   BS CIV");
+	puts("\t\t  11401234");
+	puts("\t        May 24, 2016");
+	puts("\t    rnl.lao@dlsu.edu.ph");
+	puts("\t        09983234545");
+	puts("");
+	puts("\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+return 0;
+}
